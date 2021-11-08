62c6d01bfc2d6ff2b5a5442cd2489ca41b2b797c
Ubiratan Lima <69194011+ubiratanlsc@users.noreply.github.com>
2021-11-08 06:28:04 -0300

    Update tasklist.js

--- tasklist.js
+++ tasklist.js
@@ -1,8 +1,24 @@
-const newQuest = document.querySelector('[data-form-button]')
+const criarTarefa = (evento) => {
 
+    evento.preventDefault() /* 1*/
 
-newQuest.addEventListener('click', () => {
+    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
-    console.log(valor);
-})
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${valor}</p>`
+    tarefa.innerHTML = conteudo
+
+    lista.appendChild(tarefa)
+    input.value = " "
+
+}
+
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', criarTarefa)
+
+
+
